f0a334a5cb290ec8f5bc4ec575ab81455b5768ea
Sergio Valverde <svg153@gmail.com>
2025-10-26 23:12:55 +0100

    chore: add TypeScript declaration for react-is module

---
+++ src/react-is.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-is' {
+  export function isFragment(object: any): boolean;
+  export function isLazy(object: any): boolean;
+  export function isProfiler(object: any): boolean;
+  export function isValidElementType(object: any): boolean;
+  export function isMemo(object: any): boolean;
+}

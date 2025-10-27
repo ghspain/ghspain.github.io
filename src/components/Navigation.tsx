33081ccd6880743d352b54b734c700fd5a1decc5
Sergio Valverde <svg153@gmail.com>
2025-10-27 03:02:58 +0100

    Add hidden raffle link to navigation with AnchorNav.Action

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,6 +5,9 @@ const Navigation: React.FC = () => {
   return (
     <AnchorNav>
       <AnchorNav.Link href="#inicio">Inicio</AnchorNav.Link>
+      {/* Enlace al sorteo oculto por defecto — activar cambiando el style o eliminando el display */}
+      {/* Use AnchorNav.Action to avoid AnchorNav.Link internal scrolling logic that expects a hash selector */}
+      <AnchorNav.Action href="/raffle" style={{ display: 'none' }}>Sorteo</AnchorNav.Action>
       <AnchorNav.Link href="#que-hacemos">¿Qué hacemos?</AnchorNav.Link>
       <AnchorNav.Link href="#eventos">Eventos</AnchorNav.Link>
       <AnchorNav.Link href="#quienes-somos">¿Quiénes somos?</AnchorNav.Link>
